1ec7a206c164ce265120983218283a5222abc119
Janith-Umeda <strakzzm@gmail.com>
2023-05-01 14:10:35 +0530

    fixs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import '../styles/styles.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
